c97308c4750aedc9f4ee32baa0e18b152ddc8296
vinaghost <nguyentrungnhan87@gmail.com>
2020-11-21 17:55:52 +0700

    fix syntax

--- Back-end/api/db.js
+++ Back-end/api/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
 
 const db = mysql.createConnection({
-    host: process.env.DB_HOST || "",
-    user: process.env.DB_USER || "",
-    password: process.env.DB_PASS || "",
-    database: process.env.DB_NAME || ""
+    'database': process.env.DB_NAME || '',
+    'host': process.env.DB_HOST || '',
+    'password': process.env.DB_PASS || '',
+    'user': process.env.DB_USER || ''
 });
 
 module.exports = db;
