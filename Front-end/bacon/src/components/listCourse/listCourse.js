dfdcdaa7ff09536617af22c18cce74b404e23ac3
Pham Ho Ngoc Tram <46929433+phamtram247@users.noreply.github.com>
2020-12-05 11:44:01 +0700

    Update 05/12/2020: ListCourse Page

--- Front-end/bacon/src/components/listCourse/listCourse.js
+++ Front-end/bacon/src/components/listCourse/listCourse.js
@@ -1,25 +1,91 @@
-import React, {Component}  from 'react'
+import React, {Component} from 'react' 
+import { useState } from 'react'
 import Button from 'react-bootstrap/Button'
+//import Row from 'react-bootstrap/Row'
 
 import './script.js'
 import './listCourse.css'
-<link
-  rel="stylesheet"
-  href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
-  integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
-  crossorigin="anonymous"
-/>
+import Pic1 from '../../assets/images/download.jfif'
 class listCourse extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            btnBackground: "transparent"
+        }
+    };
+    
+
+    onClickMe() {
+        alert('hhh');
+    }
 
     render() {
         return (
             <div>
-                <>
-                    <Button href="#">Link</Button> <Button type="submit">Button</Button>{' '}
-                    <Button as="input" type="button" value="Input" />{' '}
-                    <Button as="input" type="submit" value="Submit" />{' '}
-                    <Button as="input" type="reset" value="Reset" />
-                </>
+                <div class="container">
+                    <main class="grid">
+                        <article>
+                        <img src="https://picsum.photos/600/400?image=1083" alt="Sample photo"/>
+                        <div class="text">
+                            <h3>Japanese for Beginners</h3>
+                            <h4>Saint Petersburg State University</h4>                           
+                        </div>
+                        </article>
+                        <article>
+                <img src="https://picsum.photos/600/400?image=1063" alt="Sample photo"/>
+                <div class="text">
+                    <h3>How to Meditate ?</h3>
+                    <h4>New York University</h4>
+                    <div class="actionLoop visible-lg">
+				        <a onclick={this.onClickMe} class="styleBtnBuy notClick" >MUA NGAY</a>
+				    </div>
+                </div>
+                </article>
+                <article>
+                <img src="https://picsum.photos/600/400?image=1056" alt="Sample photo"/>
+                <div class="text">
+                    <h3>Michigan Programming for Everybody</h3>
+                    <h4>University of Michigan </h4>                
+                </div>
+                </article>
+                <article>
+                <img src="https://picsum.photos/600/400?image=1050" alt="Sample photo"/>
+                <div class="text">
+                    <h3>Web Development</h3>
+                    <h4>University of Michigan </h4>                 
+                </div>
+                </article>
+                <article>
+                <img src="https://picsum.photos/600/400?image=1041" alt="Sample photo"/>
+                <div class="text">
+                    <h3>Fundamentals of Finance</h3>
+                    <h4>University of Michigan </h4>                   
+                </div>
+                </article>
+                <article>
+                <img src="https://picsum.photos/600/400?image=1015" alt="Sample photo"/>
+                <div class="text">
+                    <h3>The Science of Well Being</h3>
+                    <h4>University of Michigan </h4>                 
+                </div>
+                </article>
+            </main>
+            </div>
+                        
+                    {/* <div className="lc">
+                        <div className="lc" id="lc1">
+                            <img src="../../assets/images/download.jfif"></img>
+                            <h2>Japanese for Beginners</h2>
+                        </div>
+                        <div className="lc" id="lc2">How to Meditate ?</div>       
+                        <div className="lc" id="lc3">Michigan Programming for Everybody</div>
+                    </div>
+                    <div className="lc">
+                        <div className="lc" id="lc4">Web Development</div>
+                        <div className="lc" id="lc5">Fundamentals of Finance</div>
+                        <div className="lc" id="lc6">The Science of Well Being</div>
+                    </div>   
+                       */}
             </div>
         )
     }
--- Front-end/bacon/src/containers/home-page/homepage.js
+++ Front-end/bacon/src/containers/home-page/homepage.js
@@ -3,16 +3,29 @@ import ListCourse from '../../components/listCourse/listCourse.js'
 import Page404 from '../404page/404page.js'
 import Author from '../../components/author/author.js'
 import Contact from '../../components/contact/contact.js'
-
+//mấy cái này là mình cần dùng thì mình import dô
 import './homepage.css'
 import {Switch, Route} from 'react-router-dom'
 import React, { Component } from 'react'
 import Button from 'react-bootstrap/Button'
+// để code react thì cần import "import React, { Component } from 'react'"
+// -----
+// h ví dụ m code 1 component ListCourse đi nha, làm như này
+//đầu tiền là tạo 1 component listCourse như t làm đó nè
+// xong rồi viết mấy cái cần như y cái file bên phải
+// có import React, class, render, return, trong return thì
+//viết y như code html
+//xong cuối cùng nhớ export default + tên
+//mình xuất ra rồi thì bên file homepage mình import dô như ở trên í
+// rồi mình xài bth hoy
+//oke k? ok, ma m gọi cho nhanh ko, ok
+//
 const Home = () => {
     return (
     <div>
         <Header/>
         <div id="banner"></div>
+        <ListCourse></ListCourse>
         <Author></Author>
         <Contact></Contact>
         
