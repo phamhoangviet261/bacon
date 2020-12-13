8eda00b1108cc9aa264687089d624e22c29aaace
Pham Hoang Viet <42289889+ShiroVV-hcmus@users.noreply.github.com>
2020-12-13 15:47:26 +0700

    13/12/2020: Update Overview page, fix bugs in choosing week function

--- Front-end/bacon/src/containers/overview-page/overview.js
+++ Front-end/bacon/src/containers/overview-page/overview.js
@@ -9,8 +9,8 @@ class Overview extends Component{
             overview: {
                 weekId: [1,2,3,4,5,6],
                 video: {
-                    name: ["Video 1", "Video 2"],
-                    time: ["10", "20"]
+                    name: ["Video name 1", "Video name 2", "Video name 3", "Video name 4", "Video name 5", "Video name 6"],
+                    time: ["10", "20", "10", "20", "10", "20"]
                 },
                 exam: {
                     name: ["Solving Programing", "Do it if you can"],
@@ -19,25 +19,51 @@ class Overview extends Component{
                 book: {
                     name: ["JavaScript", "Python"],
                     time: ["10", "15"]
-                }
+                },
+                idWeekPreviewing: 1
 
             }
         };
+        this.changeWeek = this.changeWeek.bind(this);
     }
 
+    showOverview(){
+        let x = document.getElementById("overview-sidebar-week");
+        if (x.style.display === "none" || x.style.display === "") {
+            x.style.display = "block";
+            // x.scrollIntoView();
+          } else {
+            x.style.display = "none";
+          }
+    }
+    componentDidMount(){
+        this.setState({
+            idWeekPreviewing: 1
+        })
+    }
+    changeWeek(item){
+        // alert(item)
+        this.setState({
+            idWeekPreviewing: item
+        })
+
+    }
 
     render(){
-        return (
+        console.log(this.state);
+        return (      
             <div id="overview-page">
                 <div id="overview-sidebar">
-                    <div className="overview-sidebar-child" id="overview-sidebar-overview">
+                    <div className="overview-sidebar-child" id="overview-sidebar-overview" onClick={this.showOverview}>
                         <div className="overview-sidebar-title"><p>Overview</p></div>
                         
                     </div>
                     <div id="overview-sidebar-week">
                         {
                             this.state.overview.weekId.map(item => (
-                                <div className="overview-sidebar-week-child">
+                                <div className="overview-sidebar-week-child"
+                                onClick={() => this.changeWeek(item)}
+                                >
                                     <p>Week</p>
                                 </div>
                             ))
@@ -62,16 +88,44 @@ class Overview extends Component{
                         <div className="overview-sidebar-title"><p>Course Info</p></div>
                     </div>
                 </div>
-                <div id="overview-container"></div>
+                <div id="overview-container">
+                        <Week
+                        weekId = {this.state.idWeekPreviewing}
+                        nameOfCourse = {this.state.name}
+                        videoname={this.state.overview.video.name[this.state.idWeekPreviewing-1]}
+                        ></Week>
+                </div>
             </div>
         )
     }
 }
 
 const Week = (props) => (
-    <div className="">
-
+    <div id="overview-container-week">
+        <div id="overview-container-week-title">
+            <h1>Week {props.weekId}</h1>
+            <h4>{props.nameOfCourse}</h4>
+        </div>
+        <div id="deobietdattengi">
+            <h2>Week {props.weekId}</h2>
+            <p>Discuss and ask questions about Week {props.weekId}.</p>
+        </div>
+        <div id="overview-container-week-warmup">
+            <h2>Warm Up</h2>
+        </div>
+        <div id="overview-container-week-learning-objective">
+            <h2>Learning Objectives</h2>
+            <li>Estimate the running time of an algorithm</li>
+            <li>Practice implementing efficient solutions</li>
+            <li>Practice solving programming challenges</li>
+            <li>Implement programs that are several orders of magnitude faster than straightforward programs</li>
+        </div>
+        <div id="overview-container-week-videoname" class="overview-container-week-videoname">
+            <h2>{props.videoname}</h2>
+        </div>
     </div>
 )
+
+
 
 export default Overview
