497dae998f595a0d65aa2314ea60d4b5fcf18a20
vinaghost <nguyentrungnhan87@gmail.com>
2020-12-15 18:22:38 +0700

    update Document, Test, Lesson route

--- Back-end/api/routes.js
+++ Back-end/api/routes.js
@@ -2,8 +2,11 @@ const memberController = require('./controllers/MemberController');
 const memberInfoController = require('./controllers/MemberInfoController');
 const courseController = require('./controllers/CourseController');
 const lessonController = require('./controllers/LessonController');
+const testController = require('./controllers/TestController');
+const documentController = require('./controllers/DocumentController');
 
 module.exports = function route (app) {
+    // Table Members
     app.route('/members/')
         .get(memberController.login)
         .post(memberController.register);
@@ -14,6 +17,7 @@ module.exports = function route (app) {
         .get(memberInfoController.show)
         .put(memberInfoController.edit);
 
+    // Table Courses
     app.use('/courses/', courseController.middleware);
     app.route('/courses/')
         .get(courseController.show)
@@ -23,7 +27,30 @@ module.exports = function route (app) {
         .get(courseController.show)
         .put(courseController.update);
 
-    app.route('/courses/:id_course/lesson/')
+    // Table Lessons
+    app.route('/courses/:id_course/lessons/')
         .get(lessonController.show)
         .post(lessonController.create);
+
+    app.route('/courses/:id_course/lessons/:id_lesson/')
+        .get(lessonController.show)
+        .post(lessonController.update);
+
+    // Table Tests
+    app.route('/courses/:id_course/tests/')
+        .get(testController.show)
+        .post(testController.create);
+
+    app.route('/courses/:id_course/tests/:id_test/')
+        .get(testController.show)
+        .post(testController.update);
+
+    // Table Documents
+    app.route('/courses/:id_course/documents/')
+        .get(documentController.show)
+        .post(documentController.create);
+
+    app.route('/courses/:id_course/documents/:id_document/')
+        .get(documentController.show)
+        .post(documentController.update);
 };
