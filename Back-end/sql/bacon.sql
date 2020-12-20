ee40b614e31f811b2d3bc7e7bf67848c6bf9a15a
vinaghost <nguyentrungnhan87@gmail.com>
2020-12-20 10:31:37 +0700

    update bacon database query

--- Back-end/sql/bacon.sql
+++ Back-end/sql/bacon.sql
@@ -15,24 +15,52 @@
 
 -- Dumping database structure for CNPM18_COURSERA_DATABASE
 DROP DATABASE IF EXISTS `CNPM18_COURSERA_DATABASE`;
-CREATE DATABASE IF NOT EXISTS `CNPM18_COURSERA_DATABASE` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE DATABASE IF NOT EXISTS `CNPM18_COURSERA_DATABASE` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `CNPM18_COURSERA_DATABASE`;
+
+-- Dumping structure for table CNPM18_COURSERA_DATABASE.Answers
+DROP TABLE IF EXISTS `Answers`;
+CREATE TABLE IF NOT EXISTS `Answers` (
+  `id_question` int NOT NULL,
+  `content` longtext COLLATE utf8mb4_unicode_ci,
+  PRIMARY KEY (`id_question`),
+  CONSTRAINT `fk_Answers_Questions` FOREIGN KEY (`id_question`) REFERENCES `Questions` (`id_question`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Data exporting was unselected.
 
 -- Dumping structure for table CNPM18_COURSERA_DATABASE.Courses
 DROP TABLE IF EXISTS `Courses`;
 CREATE TABLE IF NOT EXISTS `Courses` (
   `id_course` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) DEFAULT NULL,
-  `code` varchar(45) DEFAULT NULL,
-  `subject` varchar(255) DEFAULT NULL,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `code` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `subject` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `length` int DEFAULT NULL,
   `price` int DEFAULT NULL,
   `teacher` int DEFAULT NULL,
+  `status` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id_course`),
   UNIQUE KEY `code_UNIQUE` (`code`),
   KEY `fk_Courses_Members_idx` (`teacher`),
   CONSTRAINT `fk_Courses_Members` FOREIGN KEY (`teacher`) REFERENCES `Members` (`id_member`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table CNPM18_COURSERA_DATABASE.CoursesSold
+DROP TABLE IF EXISTS `CoursesSold`;
+CREATE TABLE IF NOT EXISTS `CoursesSold` (
+  `id_member` int NOT NULL,
+  `id_course` int NOT NULL,
+  `status` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `date_bought` datetime DEFAULT NULL,
+  `score` int DEFAULT NULL,
+  PRIMARY KEY (`id_member`,`id_course`),
+  KEY `fk_CoursesSold_Courses_idx` (`id_course`),
+  CONSTRAINT `fk_CoursesSold_Courses` FOREIGN KEY (`id_course`) REFERENCES `Courses` (`id_course`),
+  CONSTRAINT `fk_CoursesSold_Members` FOREIGN KEY (`id_member`) REFERENCES `Members` (`id_member`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Data exporting was unselected.
 
@@ -57,13 +85,13 @@ DROP TABLE IF EXISTS `Documents`;
 CREATE TABLE IF NOT EXISTS `Documents` (
   `id_document` int NOT NULL,
   `id_course` int DEFAULT NULL,
-  `type` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `type` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `date_upload` date DEFAULT NULL,
-  `content` longtext,
+  `content` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
   PRIMARY KEY (`id_document`),
   KEY `fk_Documents_Lessons_idx` (`id_course`) USING BTREE,
   CONSTRAINT `fk_Documents_Courses` FOREIGN KEY (`id_course`) REFERENCES `Courses` (`id_course`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Data exporting was unselected.
 
@@ -72,13 +100,13 @@ DROP TABLE IF EXISTS `Lessons`;
 CREATE TABLE IF NOT EXISTS `Lessons` (
   `id_lesson` int NOT NULL AUTO_INCREMENT,
   `id_course` int DEFAULT NULL,
-  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `length` int DEFAULT NULL,
-  `content` longtext,
+  `content` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
   PRIMARY KEY (`id_lesson`),
   KEY `fk_Lessons_Courses_idx` (`id_course`),
   CONSTRAINT `fk_Lessons_Courses` FOREIGN KEY (`id_course`) REFERENCES `Courses` (`id_course`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Data exporting was unselected.
 
@@ -86,15 +114,29 @@ CREATE TABLE IF NOT EXISTS `Lessons` (
 DROP TABLE IF EXISTS `Members`;
 CREATE TABLE IF NOT EXISTS `Members` (
   `id_member` int NOT NULL AUTO_INCREMENT,
-  `username` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `email` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `role` varchar(255) NOT NULL,
+  `username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `role` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id_member`),
   UNIQUE KEY `username_UNIQUE` (`username`),
   KEY `fk_Members_MembersInfo1_idx` (`id_member`),
   CONSTRAINT `fk_Members_MembersInfo` FOREIGN KEY (`id_member`) REFERENCES `MembersInfo` (`id_member`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table CNPM18_COURSERA_DATABASE.MembersAnswers
+DROP TABLE IF EXISTS `MembersAnswers`;
+CREATE TABLE IF NOT EXISTS `MembersAnswers` (
+  `id_member` int NOT NULL,
+  `id_question` int NOT NULL,
+  `score` int DEFAULT NULL,
+  PRIMARY KEY (`id_member`,`id_question`),
+  KEY `fk_MembersAnswers_Questions_idx` (`id_question`),
+  CONSTRAINT `fk_MembersAnswers_Members` FOREIGN KEY (`id_member`) REFERENCES `Members` (`id_member`),
+  CONSTRAINT `fk_MembersAnswers_Questions` FOREIGN KEY (`id_question`) REFERENCES `Questions` (`id_question`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Data exporting was unselected.
 
@@ -104,11 +146,54 @@ CREATE TABLE IF NOT EXISTS `MembersInfo` (
   `id_member` int NOT NULL AUTO_INCREMENT,
   `date_birth` date DEFAULT NULL,
   `date_registration` datetime DEFAULT NULL,
-  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
-  `sex` varchar(10) DEFAULT NULL,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `sex` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id_member`),
   UNIQUE KEY `id_member_UNIQUE` (`id_member`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table CNPM18_COURSERA_DATABASE.MembersScore
+DROP TABLE IF EXISTS `MembersScore`;
+CREATE TABLE IF NOT EXISTS `MembersScore` (
+  `id_member` int NOT NULL,
+  `id_test` int NOT NULL,
+  `score` int DEFAULT NULL,
+  PRIMARY KEY (`id_member`,`id_test`),
+  KEY `fk_MembersScore_Tests_idx` (`id_test`),
+  CONSTRAINT `fk_MembersScore_Members` FOREIGN KEY (`id_member`) REFERENCES `Members` (`id_member`),
+  CONSTRAINT `fk_MembersScore_Tests` FOREIGN KEY (`id_test`) REFERENCES `Tests` (`id_test`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table CNPM18_COURSERA_DATABASE.Questions
+DROP TABLE IF EXISTS `Questions`;
+CREATE TABLE IF NOT EXISTS `Questions` (
+  `id_question` int NOT NULL AUTO_INCREMENT,
+  `id_test` int DEFAULT NULL,
+  `content` longtext COLLATE utf8mb4_unicode_ci,
+  `score` int DEFAULT NULL,
+  PRIMARY KEY (`id_question`),
+  KEY `fk_Questions_Tests_idx` (`id_test`),
+  CONSTRAINT `fk_Questions_Tests` FOREIGN KEY (`id_test`) REFERENCES `Tests` (`id_test`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table CNPM18_COURSERA_DATABASE.Reviews
+DROP TABLE IF EXISTS `Reviews`;
+CREATE TABLE IF NOT EXISTS `Reviews` (
+  `id_member` int NOT NULL,
+  `id_course` int NOT NULL,
+  `star` int DEFAULT NULL,
+  `content` longtext COLLATE utf8mb4_unicode_ci,
+  PRIMARY KEY (`id_member`,`id_course`),
+  KEY `fk_Rwviews_Courses_idx` (`id_course`),
+  CONSTRAINT `fk_Reviews_Members` FOREIGN KEY (`id_member`) REFERENCES `Members` (`id_member`),
+  CONSTRAINT `fk_Rwviews_Courses` FOREIGN KEY (`id_course`) REFERENCES `Courses` (`id_course`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Data exporting was unselected.
 
@@ -117,13 +202,13 @@ DROP TABLE IF EXISTS `Tests`;
 CREATE TABLE IF NOT EXISTS `Tests` (
   `id_test` int NOT NULL,
   `id_course` int DEFAULT NULL,
-  `name` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
-  `content` longtext,
+  `name` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `content` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
   `length` int DEFAULT NULL,
   PRIMARY KEY (`id_test`),
   KEY `fk_test_lesson_idx` (`id_course`) USING BTREE,
-  CONSTRAINT `fk_course_lesson` FOREIGN KEY (`id_course`) REFERENCES `Courses` (`id_course`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  CONSTRAINT `fk_Tests_Courses` FOREIGN KEY (`id_course`) REFERENCES `Courses` (`id_course`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Data exporting was unselected.
 
