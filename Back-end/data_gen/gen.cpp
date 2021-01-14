fe5bda85319be9eb7f94fbdd9982d785f75f2b02
vinaghost <nguyentrungnhan87@gmail.com>
2021-01-14 16:22:05 +0700

    fix missing arg in gen-or data

--- Back-end/data_gen/gen.cpp
+++ Back-end/data_gen/gen.cpp
@@ -5,8 +5,8 @@
 std::string create_user(int index) {
     std::stringstream ss;
     //                  username password email
-    // CALL `create_user`('?', '_', '?');
-    ss << "CALL `create_user`('vinabot" << index << "', '64e3d8c99a47e902a56ba96f1f89df3f8b42d6e9691dfbe0b05f13449d2b2636','vinabot" << index << "@vina.ghost');";
+    // CALL `create_user`('?', '_', '?', 'Member');
+    ss << "CALL `create_user`('vinabot" << index << "', '64e3d8c99a47e902a56ba96f1f89df3f8b42d6e9691dfbe0b05f13449d2b2636','vinabot" << index << "@vina.ghost', 'Member');";
     
     return std::string(ss.str());
 }
