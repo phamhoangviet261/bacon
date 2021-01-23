f928e4a4f607f0bc91ce4e6c92c1ad997a2f91c2
VINAGHOST <nguyentrungnhan87@gmail.com>
2021-01-23 14:42:48 +0700

    add cors to backend

--- Back-end/server.js
+++ Back-end/server.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 require('dotenv').config({ path: './.env' });
 
 const port = process.env.PORT || 3000;
+app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
